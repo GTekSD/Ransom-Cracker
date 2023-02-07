ba70b5d001bec9ce1b1ed4db9bf85172cb87ab70
Сухас Дхолз <55411358+GTekSD@users.noreply.github.com>
2023-02-07 16:19:19 +0530

    Create ransom-cli.py

---
+++ beta/ransom-cli.py
@@ -0,0 +1,47 @@
+import zipfile
+
+def crack_password(password_list, obj):
+
+    # tracking line no. at which password is found
+    idx = 0
+ 
+    # open file in read byte mode only as "rockyou.txt"
+    # file contains some special characters and hence
+    # UnicodeDecodeError will be generated
+    with open(password_list, 'rb') as file:
+        for line in file:
+            for word in line.split():
+                try:
+                    idx += 1
+                    obj.extractall(pwd=word)
+                    print(" ")
+                    print("[+] Performing password attack on",zip_file)
+                    print(" ")
+                    print("[!] Valid Combinations Found: line =", idx, "/" , cnt)
+                    print(" |  Password:", word.decode())
+                    return True
+                except:
+                    print(f"[-] Attempt {idx}/{cnt} with password {word.decode()} failed.")
+                    continue
+    return False
+ 
+ 
+# Enter encrepted zip file name here.
+zip_file = input("Encrypted .zip file name:")
+
+# Replace password file with any other if password not found.
+password_list = input("Passwds list:")
+
+# ZipFile object initialised
+obj = zipfile.ZipFile(zip_file)
+ 
+# count of number of words present in file
+cnt = len(list(open(password_list, "rb")))
+ 
+if crack_password(password_list, obj) == False:
+    print(" ")
+    print("[i] No Valid Passwords Found.")
+    print(" |  There are total", cnt, "number of passwords combinations are not valid.")
+    print("[+] _______________________________________________________________________")
+    print()
+    print("[!] Try different wordlist from SecLists/Passwords")
